ac7c6a76ea22fae083fc78cc7e92c6ae872d6d8e
zengzheng <zengzheng2009@gmail.com>
2017-10-07 23:03:35 +0800

    add form html & form js

---
+++ js/form.js
@@ -0,0 +1,27 @@
+var elForm, elSelectPackage, elPackageHint, elTerms, elTermsHint;
+elForm          = document.getElementById('formSignup');
+elSelectPackage = document.getElementById('package');
+elPackageHint   = document.getElementById('packageHint');
+elTerms         = document.getElementById('terms');
+elTermsHint     = document.getElementById('termsHint');
+
+
+function packageHint() {
+  var pack = this.options[this.selectedIndex].value;
+  if (pack === 'monthly') {
+    elPackageHint.innerHTML = 'Save $10 if you pay for 1 year!';
+  } else {
+    elPackageHint.innerHTML = 'Wise choice';
+  }
+}
+
+function checkTerms(event) {
+  if (!elTerms.checked) {
+    elTermsHint.innerHTML = 'You must agree to the terms.';
+    event.preventDefault();
+  }
+}
+
+
+elFrom.addEventListener('submit', checkTerms, false);
+elSelectPackage.addEventListener('change', packageHint, false);
